e8fdb4a9d22f59460ab4fe1566f4f04108f72907
NayanPunyarthi <punyarthinayan16@gmail.com>
2021-09-02 21:24:41 +0530

    Snake and Ladder

---
+++ src/UC1.java
@@ -0,0 +1,10 @@
+//Single Player
+public class UC1 {
+
+        public static void main(String[] args) {
+            System.out.println("welcome to snake and ladder game");
+            int playposition = 0 ;
+        }
+    }
+
+
---
+++ src/UC2.java
@@ -0,0 +1,10 @@
+//Die Rolls
+public class UC2 {
+        public static void main(String[] args) {
+            int position=0;
+            int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Die Rolled:"+dieRoll);
+        }
+
+
+    }
